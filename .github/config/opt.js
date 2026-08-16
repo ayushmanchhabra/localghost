b4b771a16f8ab35c91cc9d333d0aa1f7728c74f7
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-08-16 11:55:52 +0530

    fix: set platform and arch

--- .github/config/opt.js
+++ .github/config/opt.js
@@ -1,6 +1,22 @@
+import process from "node:process";
+
+const PLATFORM_KV = {
+  darwin: "osx",
+  linux: "linux",
+  win32: "win",
+};
+
+const ARCH_KV = {
+  x64: "x64",
+  ia32: "ia32",
+  arm64: "arm64",
+};
+
 const options = {
     version: "0.114.1",
     flavor: "sdk",
+    platform: PLATFORM_KV[process.platform],
+    arch: ARCH_KV[process.arch],
     downloadUrl: "https://dl.nwjs.io",
     manifestUrl: "https://nwjs.io/versions.json",
     cacheDir: "./cache",
